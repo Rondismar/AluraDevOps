4212672f9fd65bf140d700a8595168b64ed7a741
Rondismar Gomes <rondismar@gmail.com>
2022-04-23 18:08:07 -0300

    add vm phpweb and script to install puppet

--- src/index.php
+++ src/index.php
@@ -1,3 +1,15 @@
 <?php
-echo "PHP instalado com sucesso!!!"
+echo "Testando conexao <br /> <br />";
+$servername = "10.0.0.20";
+$username = "phpuser";
+$password = "pass";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+echo "Conectado com sucesso";
 ?>
