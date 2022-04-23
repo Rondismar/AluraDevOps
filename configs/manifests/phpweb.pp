e8f06569d109fa6176bfc5b34070a5c31c1f5d5c
Rondismar Gomes <rondismar@gmail.com>
2022-04-23 02:08:10 -0300

    add script shell e provisioner

---
+++ configs/manifests/phpweb.pp
@@ -0,0 +1,13 @@
+# execute 'apt-get update'
+exec {'apt-update':
+    command => '/usr/bin/apt-get update'
+    }
+package { ['php7.2' ,'php7.2-mysql'] :
+    require => Exec['apt-update'],
+    ensure => installed,
+}
+
+exec { 'run-php7':
+    require => Package['php7.2'],
+    command => '/usr/bin/php -S 10.0.0.155:8888 -t /vagrant/src &'
+}    
